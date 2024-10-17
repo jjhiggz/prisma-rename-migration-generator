a59c85e81726b0298c60101350e4b287f4e68c3a
Jon Higger <jonathan.higger@gmail.com>
2024-10-16 21:18:45 -0600

    had a little mistake in the logic that made it only work for restaurants

--- index.ts
+++ index.ts
@@ -11,76 +11,74 @@ const models = prismaNode.rootNode.children.filter(
   (n) => n.type === "model_declaration"
 );
 
-const restaurant = models.find((model) => {
-  return model.children.find((child) => {
-    return child.type === "identifier" && child.text === "Restaurant";
-  });
-});
-
 type ModelGroups = Record<
   "model" | "identifier" | "statement_block",
   TreeSitter.SyntaxNode[]
 >;
 
-const modelGroups = groupBy(
-  restaurant?.children ?? [],
-  (child) => child.type
-) as any as ModelGroups;
+const modelGroups = models.map(
+  (model) =>
+    groupBy(model.children ?? [], (child) => child.type) as any as ModelGroups
+);
 
-const modelName = modelGroups.identifier[0].text;
-const modelBlock = modelGroups.statement_block[0];
+const sqlStrings = modelGroups.map((modelGroup) => {
+  const modelName = modelGroup.identifier[0].text;
+  const modelBlock = modelGroup.statement_block[0];
 
-type BlockGroups = Record<
-  | "{"
-  | "}"
-  | "column_declaration"
-  | "developer_comment"
-  | "block_attribute_declaration",
-  TreeSitter.SyntaxNode[]
->;
-
-const blockGroups = groupBy(
-  modelBlock.children,
-  (child) => child.type
-) as any as BlockGroups;
-
-const columnData = blockGroups.column_declaration.map((column) => {
-  type ColumnGroups = Record<
-    "identifier" | "column_type" | "attribute",
+  type BlockGroups = Record<
+    | "{"
+    | "}"
+    | "column_declaration"
+    | "developer_comment"
+    | "block_attribute_declaration",
     TreeSitter.SyntaxNode[]
   >;
-  const columnGroups = groupBy(
-    column.children,
+
+  const blockGroups = groupBy(
+    modelBlock.children,
     (child) => child.type
-  ) as any as ColumnGroups;
+  ) as any as BlockGroups;
 
-  const currentName = columnGroups.identifier[0].text;
+  const columnData = blockGroups.column_declaration.map((column) => {
+    type ColumnGroups = Record<
+      "identifier" | "column_type" | "attribute",
+      TreeSitter.SyntaxNode[]
+    >;
+    const columnGroups = groupBy(
+      column.children,
+      (child) => child.type
+    ) as any as ColumnGroups;
 
-  const oldName = columnGroups.attribute
-    ?.map((attr) => {
-      const attrText = attr.text;
-      if (!attrText.startsWith("@map")) return undefined;
-      const text = attrText.split('"');
-      const oldName = text[1];
-      if (oldName === currentName) {
-        return undefined;
-      }
-      return oldName;
-    })
-    .find((n) => n);
-  return {
-    currentName,
-    oldName,
-    modelName,
-  };
+    const currentName = columnGroups.identifier[0].text;
+
+    const oldName = columnGroups.attribute
+      ?.map((attr) => {
+        const attrText = attr.text;
+        if (!attrText.startsWith("@map")) return undefined;
+        const text = attrText.split('"');
+        const oldName = text[1];
+        if (oldName === currentName) {
+          return undefined;
+        }
+        return oldName;
+      })
+      .find((n) => n);
+    return {
+      currentName,
+      oldName,
+      modelName,
+    };
+  });
+
+  let sqlString = ``;
+
+  for (const column of columnData) {
+    if (!column.oldName) continue;
+    sqlString += `ALTER TABLE "${column.modelName}" RENAME COLUMN "${column.oldName}" TO "${column.currentName}";\n`;
+  }
+
+  return sqlString;
 });
-
-let sqlString = ``;
-
-for (const column of columnData) {
-  if (!column.oldName) continue;
-  sqlString += `ALTER TABLE "${column.modelName}" RENAME COLUMN "${column.oldName}" TO "${column.currentName}";\n`;
-}
 
 const newPrismaFile = oldFile.replace(/\ @map\(.*\)/g, "");
 
@@ -93,7 +91,7 @@ await writeFile("./build/schema.prisma", newPrismaFile, {
 }).then(() => {
   console.log("wrote new schema.prisma in build/schema.prisma");
 });
-await writeFile("./build/migration.sql", sqlString, {
+await writeFile("./build/migration.sql", sqlStrings.join("\n"), {
   encoding: "utf-8",
 }).then(() => {
   console.log("wrote new migration.sql in build/migration.sql");
